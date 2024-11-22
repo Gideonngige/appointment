ca319b26aff447a6e1f3d943b8f0f1d269124cef
Gideonngige <ushindigideon01@gmail.com>
2024-11-23 02:09:06 +0300

    Second commit

---
+++ appointment.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>appointment</title>
+</head>
+<body>
+    <div class="container">
+        <h4>Book Appointment</h4>
+        <form>
+            <label>Choose an activity</label>
+            <select class="form-control" name="appointment">
+                <option value="">--Select an activity--</option>
+                <option>Cabinet Appointments</option>
+                <option>Judicial Appointments</option>
+                <option>Insepector-General of Police</option>
+                <option>Commisioners of Independent Offices</option>
+
+            </select><br>
+            <input type="submit" value="Book Appointment" class="btn btn-primary" />
+        </form>
+
+    </div>
+    
+</body>
+</html>
--- register.php
+++ register.php
@@ -6,20 +6,28 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="login.css">
-    <title>login</title>
+    <title>register</title>
 </head>
 <body>
     <div class="container">
         <div class="inner-container">
             <form action="" method="POST">
-            <h4 class="header">Login</h4><hr>
+            <h4 class="header">Register</h4><hr>
+            <table>
+                <tr>
+                    <td><label>First name</label><br><input type="text" class="form-control" placeholder="your first name" /></td>
+                    <td><label>Last name</label><br><input type="text" class="form-control" placeholder="your last name" /></td>
+                </tr>
+            </table>
             <label>Email</label><br>
             <input type="email" name="email" class="form-control" placeholder="e.g example@gmail.com" required /><br>
             <label>Password</label><br>
             <input type="password" name="password" class="form-control" placeholder="your password" required /><br>
-            <p>Forgot password?<a href="" style="color:black">Reset here</a></p>
-            <input type="submit" value="Login" class="btn btn-custom" />
-            <p>Do not have account?<a href="register.php" style="color:black">Sign Up</a></p>
+            <label>Repeat password</label><br>
+            <input type="password" name="password" class="form-control" placeholder="Repeat your password" required /><br>
+            <p>Already have an account?<a href="login.php" style="color:black">Sign In</a></p>
+            <input type="submit" value="Register"  class="btn btn-custom" />
+            
             </form>
         </div>
     </div>
